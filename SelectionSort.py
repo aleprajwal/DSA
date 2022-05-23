1b4efe33a642a82d5a84b0b5263bab6f3aa68e14
aleprajwal <aleprajwal11@gmail.com>
2022-05-23 22:59:30 +1000

    initial commit

---
+++ SelectionSort.py
@@ -0,0 +1,14 @@
+
+class Solution():
+    """
+    Implementation of Selection Sorting Algorithm
+    """
+
+    def selectionSort(self, nums):
+        pass
+
+nums = [-9,10,3,1,22,0]
+solution = Solution()
+result = solution.selectionSort(nums)
+
+print(result)
